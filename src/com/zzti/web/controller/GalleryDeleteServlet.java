a0175534e2ff58fed3acbe06388b36472fcc46b7
Michael.Tian <michael.tian@loweprofero.com>
2015-06-26 15:51:35 +0800

    add gallery delete method.

--- src/com/zzti/web/controller/GalleryDeleteServlet.java
+++ src/com/zzti/web/controller/GalleryDeleteServlet.java
@@ -1,11 +1,18 @@
 package com.zzti.web.controller;
 
-import com.zzti.common.HttpBaseServlet;
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.zzti.bean.Gallery;
+import com.zzti.bean.Result;
+import com.zzti.business.GalleryBusiness;
+import com.zzti.common.HttpBaseServlet;
+import com.zzti.utils.RegexUtil;
 
 /**
  * Servlet implementation class GalleryDeleteServlet
@@ -27,6 +34,26 @@ public class GalleryDeleteServlet extends HttpBaseServlet {
 	 */
 	protected void doDeal(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String id = request.getParameter("id");
+		// 判断参数是否正确
+		if (!RegexUtil.isInteger(id)) {
+			request.setAttribute("message", "参数错误！");
+			request.getRequestDispatcher("/WEB-INF/jsp/message.jsp").forward(request,
+					response);
+			return;
+		}
+		
+		Gallery data = new Gallery();
+		data.setId(Integer.parseInt(id));
+		Result result = new GalleryBusiness().delete(data);
+		if(result.getResult()!=1)
+		{
+			request.setAttribute("message", result.getMessage());
+			request.getRequestDispatcher("/WEB-INF/jsp/message.jsp").forward(request,
+					response);
+			return;
+		}
+		response.sendRedirect(request.getContextPath()+"/servlet/IndexUIServlet");
 	}
 
 }
