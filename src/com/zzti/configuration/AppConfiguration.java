223e05a15ad2cecc745c78a44dedf19fbf2513d3
Michael.Tian <michael.tian@loweprofero.com>
2016-03-01 18:37:52 +0800

    add favicon.ico

--- src/com/zzti/configuration/AppConfiguration.java
+++ src/com/zzti/configuration/AppConfiguration.java
@@ -57,6 +57,7 @@ public class AppConfiguration extends WebMvcConfigurerAdapter{
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
         registry.addResourceHandler("/font/**").addResourceLocations("/font/");
         registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
     }
      
     @Bean
